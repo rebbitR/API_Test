e140e46c919b328e884b34b2ec13a7a0e07a68b7
rebbitR <renakdev@gmail.com>
2022-10-21 15:02:10 +0300

    Update Selenium.py
    add notes

--- Selenium.py
+++ Selenium.py
@@ -5,32 +5,47 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 import time
 
+# chromedriver
 service = ChromeService(executable_path=ChromeDriverManager().install())
 driver = webdriver.Chrome(service=service)
+# search in google chrome: https://www.demoblaze.com/index.html
 driver.get('https://www.demoblaze.com/index.html')
+# maximize window browser
 driver.maximize_window()
+# click on 'Log in' tab
 element=driver.find_element("id","login2")
 element.click()
 time.sleep(2)
+# send the username and password
 username='Renana'
 password='12345678'
 driver.find_element(By.ID,"loginusername").send_keys(username)
 driver.find_element(By.ID,"loginpassword").send_keys(password)
 time.sleep(1)
+# find the 'Log in' button an click on it
 driver.find_element(By.XPATH, '//button[text()="Log in"]').click()
-time.sleep(2)
+time.sleep(3)
+# choose the product Nexus 6
 driver.find_element(By.XPATH, '//*[@id="tbodyid"]/div[3]/div/a/img').click()
 time.sleep(2)
+# add to cart (find the 'Add to cart' button an click on it)
 driver.find_element(By.XPATH, '//*[@id="tbodyid"]/div[2]/div/a').click()
+# click on 'Cart' tab
 driver.find_element(By.XPATH, '//*[@id="cartur"]').click()
 time.sleep(2)
+# find the list of items in the cart
 card=driver.find_element(By.XPATH,'//*[@id="tbodyid"]')
 items=card.find_elements(By.TAG_NAME,'tr')
+# check if number of items in the card == 1
 if len(items)==1:
-    item=items[0]
+    # pop the element in the cart
+    item=items.pop()
+    # find the properties list of element by tag name
     properties=item.find_elements(By.TAG_NAME,'td')
-    name=properties[1]
-    if name.text=='Nexus 6':
+    # check if the title of the selected phone == “Nexus 6”
+    title=properties[1]
+    if title.text=='Nexus 6':
+        # check if the price of the selected phone == 650
         price=properties[2]
         if price.text=='650':
             print('success!!!')
